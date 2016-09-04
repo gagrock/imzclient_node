fe95a14519c591072d10f5a9798e98eee86d4123
Irshad Ahmad Sheikh <irshsheikh@gmail.com>
2016-09-04 19:53:02 +0530

    modular server

--- server.js
+++ server.js
@@ -1,31 +1,51 @@
 var express = require('express');
-var app = express();
+var fs      = require('fs');
 var path = require('path');
+//var app = express();
 
-app.use('/css',express.static(path.join(__dirname, '/css')));
-app.use('/js',express.static(path.join(__dirname, '/js')));
-app.use('/img',express.static(path.join(__dirname, '/img')));
-app
-	.get('/', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/templates/home.html'));
-	})
-	.get('/explore', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/templates/explore.html'));
-	})
-	.get('/signup', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/templates/signup.html'));
-	})
-	.get('/editor', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/templates/editor.html'));
-	})
-	.get('/articles', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/templates/userArticles.html'));
-	})
-	.get('/account', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/templates/account.html'));
-	})
-	.get('/article', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/templates/article.html'));
-	})
+function server(){
+	this.app = express();
+	this.port = process.env.OPENSHIFT_NODEJS_PORT || 8010;
+	this.ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+    this.configRoutesDirs = function(){ 
+    	 
+    	 this.app.use('/css',express.static(path.join(__dirname, '/css')))
+			    .use('/js',express.static(path.join(__dirname, '/js')))
+				.use('/img',express.static(path.join(__dirname, '/img')))
+				.get('/', function(req, res) {
+			    	res.sendFile(path.join(__dirname + '/templates/home.html'));
+				})
+				.get('/explore', function(req, res) {
+			    	res.sendFile(path.join(__dirname + '/templates/explore.html'));
+				})
+				.get('/signup', function(req, res) {
+			    	res.sendFile(path.join(__dirname + '/templates/signup.html'));
+				})
+				.get('/editor', function(req, res) {
+			    	res.sendFile(path.join(__dirname + '/templates/editor.html'));
+				})
+				.get('/articles', function(req, res) {
+			    	res.sendFile(path.join(__dirname + '/templates/userArticles.html'));
+				})
+				.get('/account', function(req, res) {
+			    	res.sendFile(path.join(__dirname + '/templates/account.html'));
+				})
+				.get('/article', function(req, res) {
+			    	res.sendFile(path.join(__dirname + '/templates/article.html'));
+				});
+		   
+			};
+	this.start = function(){
+			 var port = this.port;
+			 var ip = this.ip_address;		
+			  this.app.listen(port, ip, function () {
+				  console.log( "Listening on " + ip + ", server_port " + port);
+				});
+	};
 
-  .listen(8080);
+} //
+
+
+var AppServer = new server();
+	AppServer.configRoutesDirs();
+	AppServer.start();
